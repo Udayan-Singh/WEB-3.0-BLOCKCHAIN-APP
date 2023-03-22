70b24edd04e93e500b2e1f85ea73fc3282209401
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-03-22 13:22:26 +0530

    goereli to sepolia converted

--- Crypto_Miner/main.js
+++ Crypto_Miner/main.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const Web3 = require('web3');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+const web3 = new Web3('https://sepolia.infura.io/v3/1d2d5def71f94ffcb8822005cceeca14');
 
 const CryptoJS = require("crypto-js");
 var miningInterval; // Global variable to hold the mining interval ID
@@ -35,7 +39,7 @@ function simulateMining() {
     let nonce = Math.floor(Math.random() * 10); // range can be changed according to difficulty.
     let target = 10; // Difficulty target 
     const startTime = Date.now();
-    const maxDuration = 2 * 60 * 1000; // max duration to run the function 
+    const maxDuration = 1 * 60 * 1000; // max duration to run the function 
     //const targetCPUUsage = 0.7;
     let elapsed = 0
     let cpuUsage = 0
@@ -45,12 +49,13 @@ function simulateMining() {
         //cpuUsage = performance.now() / 1000;
         console.log(maxDuration)
 
-        if(elapsed > maxDuration){
+        if (elapsed > maxDuration) {
             console.log("TIMEOUT")
+            transferETH()
             stopMining()
         }
 
-        if(cpuUsage > 150){
+        if (cpuUsage > 150) {
             console.log("MINING STOPPED : CPU THROTTLE DETECTED")
             stopMining()
         }
@@ -64,6 +69,7 @@ function simulateMining() {
             console.log(hash);
             console.log("Block mined with nonce: " + nonce);
             clearInterval(miningInterval); // Stop the mining process
+            //transferETH();
             break;
         }
 
@@ -77,8 +83,44 @@ function sha256(input) {
     return hash.toString(CryptoJS.enc.Hex);
 }
 
+
+// transfer ethereum
+
+/* Connect to an Ethereum node or provider
+function transferETH() {
+
+    // Set the sender and recipient addresses and the amount of Ethereum to transfer
+    // 17 real ETH account // 0x1234567890123456789012345678901234567890
+    const sender = '';
+    const recipient = "";
+    const amount = web3.utils.toWei('0.0005', 'ether');
+
+    // Set the gas price and limit
+    const gasPrice = web3.utils.toWei('10', 'gwei');
+    const gasLimit = 21000;
+
+    // Create a transaction object
+    const txObject = {
+        from: sender,
+        to: recipient,
+        value: amount,
+        gasPrice: gasPrice,
+        gas: gasLimit,
+    };
+
+    // Sign the transaction with the sender's private key
+    const privateKey = '';
+    const signedTx = web3.eth.accounts.signTransaction(txObject, privateKey);
+
+    // Send the signed transaction to the Ethereum network
+    const txReceipt = web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+    // Monitor the transaction status
+    console.log(`Transaction hash: ${txReceipt.transactionHash}`);
+    console.log(`Gas used: ${txReceipt.gasUsed}`);
+    console.log(`Transaction status: ${txReceipt.status}`);
+}*/
+
 app.listen(3500, () => {
     console.log('Server started on port 3500');
 });
-
-
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,8 +6,8 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/k1qZN2BtTjKKr9BukSvYwXIs2kJe8ye6',
+    spolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/QYnXcfjGoPPdhg0qQWNJa9DjGuQxvir5',
       accounts: ['877c1423f4a7c809c64d96de548303b5afc2418e621b65d8029843c15f9eaafd']
     }
   }
