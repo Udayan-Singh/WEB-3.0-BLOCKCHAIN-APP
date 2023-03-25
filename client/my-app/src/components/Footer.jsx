d69785b32af438cb1b841003fb2691211a4bd5aa
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-03-25 20:40:32 +0530

    minor changes

--- client/my-app/src/components/Footer.jsx
+++ client/my-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ let year = time.getFullYear();
 
 const Footer = ()=>{
     return (<div className="footerstyle">
-        <p>© {year} ETHERUNNER &nbsp; All Rights Reserved.</p>
+        <p>© {year} ETH ~ flow &nbsp; All Rights Reserved.</p>
     </div>)
 }
 
