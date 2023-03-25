2f4c766c1aaf2c2ab2fcdc00593438382d019dff
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-03-25 18:27:42 +0530

    prices ui improved

--- prices/src/App.js
+++ prices/src/App.js
@@ -21,29 +21,33 @@ export default function App() {
       .get("https://api.coingecko.com/api/v3/coins/")
       .then((response) => setData(response.data))
       .catch((error) => console.error(error));
+      
+      https://media1.giphy.com/media/grNkIEN4dkiMXFLIE9/giphy.gif?cid=ecf05e47ikavad7chm40zvfm86xu1rd2mfhswgm5klw92jnw&rid=giphy.gif&ct=s
    */
+
+  /* https://media0.giphy.com/media/EJNm9rPe4J8FSIfZqm/giphy.gif?cid=ecf05e47yft84zza5x0e4gj4uotxmerl8fevi1dyqzxckp15&rid=giphy.gif&ct=s */
   if (!data) {
     return (<div className="loadingscreen">
       <h1 className="fetching">Fetching data ...</h1>
-      <img src="https://media1.giphy.com/media/grNkIEN4dkiMXFLIE9/giphy.gif?cid=ecf05e47ikavad7chm40zvfm86xu1rd2mfhswgm5klw92jnw&rid=giphy.gif&ct=s" alt="loader" />
+      <img src="https://media0.giphy.com/media/EJNm9rPe4J8FSIfZqm/giphy.gif?cid=ecf05e47yft84zza5x0e4gj4uotxmerl8fevi1dyqzxckp15&rid=giphy.gif&ct=s" alt="loader" />
     </div>);
   }
 
   return (
     <div>
       <table>
-        <tr>
+        <tr className="stickyHead">
           <th>Logo</th>
           <th>Name</th>
           <th>Symbol</th>
-          <th>Price $</th>
-          <th>Price ₹</th>
-          <th>Price Change 24hr $</th>
-          <th>Price Change 24hr ₹</th>
-          <th>24 hr High ($)</th>
-          <th>24 hr High (₹)</th>
-          <th>24 hr Low ($)</th>
-          <th>24 hr Low (₹)</th>
+          <th>Price ($)</th>
+          <th>Price (₹)</th>
+          <th>Change 24h ($)</th>
+          <th>Change 24h (₹)</th>
+          <th>24h High ($)</th>
+          <th>24h High (₹)</th>
+          <th>24h Low ($)</th>
+          <th>24h Low (₹)</th>
         </tr>
         {data.map((data) => (
           <tr className="newrow">
@@ -52,6 +56,7 @@ export default function App() {
             </td>
             <td className="coinName">{data.name}</td>
             <td className="symbolName">{data.symbol.toUpperCase()}</td>
+  
             <td className="prices">{data.market_data.current_price.usd}</td>
             <td className="prices">{data.market_data.current_price.inr}</td>
 
