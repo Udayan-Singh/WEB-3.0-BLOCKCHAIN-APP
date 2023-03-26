cd394934e1f6c0030176e426728e40ffe7d0ddbf
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-03-26 20:10:38 +0530

    conditional rendering done

--- prices/src/App.js
+++ prices/src/App.js
@@ -27,10 +27,15 @@ export default function App() {
 
   /* https://media0.giphy.com/media/EJNm9rPe4J8FSIfZqm/giphy.gif?cid=ecf05e47yft84zza5x0e4gj4uotxmerl8fevi1dyqzxckp15&rid=giphy.gif&ct=s */
   if (!data) {
-    return (<div className="loadingscreen">
-      <h1 className="fetching">Fetching data ...</h1>
-      <img src="https://media0.giphy.com/media/EJNm9rPe4J8FSIfZqm/giphy.gif?cid=ecf05e47yft84zza5x0e4gj4uotxmerl8fevi1dyqzxckp15&rid=giphy.gif&ct=s" alt="loader" />
-    </div>);
+    return (
+      <div className="loadingscreen">
+        <h1 className="fetching">Fetching data ...</h1>
+        <img
+          src="https://media0.giphy.com/media/EJNm9rPe4J8FSIfZqm/giphy.gif?cid=ecf05e47yft84zza5x0e4gj4uotxmerl8fevi1dyqzxckp15&rid=giphy.gif&ct=s"
+          alt="loader"
+        />
+      </div>
+    );
   }
 
   return (
@@ -42,12 +47,20 @@ export default function App() {
           <th>Symbol</th>
           <th>Price ($)</th>
           <th>Price (₹)</th>
+
+          <th>Market Cap($)</th>
+          <th>Market Cap(₹)</th>
+
           <th>Change 24h ($)</th>
           <th>Change 24h (₹)</th>
           <th>24h High ($)</th>
           <th>24h High (₹)</th>
           <th>24h Low ($)</th>
           <th>24h Low (₹)</th>
+
+          <th>7 Days</th>
+          <th>30 Days</th>
+          <th>1 Year</th>
         </tr>
         {data.map((data) => (
           <tr className="newrow">
@@ -56,21 +69,59 @@ export default function App() {
             </td>
             <td className="coinName">{data.name}</td>
             <td className="symbolName">{data.symbol.toUpperCase()}</td>
-  
-            <td className="prices">{data.market_data.current_price.usd}</td>
-            <td className="prices">{data.market_data.current_price.inr}</td>
 
-            <td className="Gprices">{Math.floor(data.market_data.price_change_24h_in_currency.usd)}</td>
-            <td className="Gprices">{Math.floor(data.market_data.price_change_24h_in_currency.inr)}</td>
+            <td className="prices">${data.market_data.current_price.usd}</td>
+            <td className="prices">₹{data.market_data.current_price.inr}</td>
 
-            <td className="additional">{data.market_data.high_24h.usd}</td>
-            <td className="additional">{data.market_data.high_24h.inr}</td>
-            <td className="additional">{data.market_data.low_24h.usd}</td>
-            <td className="additional">{data.market_data.low_24h.inr}</td>
+            <td className="marketcap">${data.market_data.market_cap.usd}</td>
+            <td className="marketcap">₹{data.market_data.market_cap.inr}</td>
+
+            {data.market_data.price_change_24h_in_currency.usd < 0 ? (
+              <td className="Rprices">
+              ${Math.round(data.market_data.price_change_24h_in_currency.usd * Math.pow(10, 5)) / Math.pow(10, 5)}
+              </td>
+            ) : (
+              <td className="Gprices">
+              ${Math.round(data.market_data.price_change_24h_in_currency.usd * Math.pow(10, 5)) / Math.pow(10, 5)}
+              </td>
+            )}
+
+            {data.market_data.price_change_24h_in_currency.inr < 0 ? (
+              <td className="Rprices">
+              ₹{Math.round(data.market_data.price_change_24h_in_currency.inr * Math.pow( 10, 5 ))/Math.pow( 10, 5 )}
+              </td>
+            ) : (
+              <td className="Gprices">
+              ₹{Math.round(data.market_data.price_change_24h_in_currency.inr * Math.pow( 10, 5 ))/Math.pow( 10, 5 )}
+              </td>
+            )}
+
+            <td className="additional">${data.market_data.high_24h.usd}</td>
+            <td className="additional">₹{data.market_data.high_24h.inr}</td>
+            <td className="additional">${data.market_data.low_24h.usd}</td>
+            <td className="additional">₹{data.market_data.low_24h.inr}</td>
+
+            {data.market_data.price_change_percentage_7d > 0 ? (
+              <td className="Gprices">{data.market_data.price_change_percentage_7d}% </td>
+            ):(
+              <td className="Rprices">{data.market_data.price_change_percentage_7d}% </td>
+            )}
+
+            {data.market_data.price_change_percentage_30d > 0 ? (
+              <td className="Gprices">{data.market_data.price_change_percentage_30d}% </td>
+            ):(
+              <td className="Rprices">{data.market_data.price_change_percentage_30d}% </td>
+            )}
+
+            {data.market_data.price_change_percentage_1y > 0 ? (
+              <td className="Gprices">{data.market_data.price_change_percentage_1y}% </td>
+            ):(
+              <td className="Rprices">{data.market_data.price_change_percentage_1y}% </td>
+            )}
+            
           </tr>
         ))}
       </table>
-
     </div>
   );
 }
